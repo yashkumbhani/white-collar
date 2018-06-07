f971df9529f755b2a767106e3f72c644d4a5fcfd
Yash Kumbhani <ykumbhani@sapient.com>
2018-06-07 16:13:14 +0100

    moved price difference to ENUMS

--- decider.js
+++ decider.js
@@ -2,7 +2,7 @@ const operations = require('./operations');
 const livePosition = require('./utils/live-position');
 const shouldClose = require('./utils/should-close');
 
-const {QUANTITY} = require('./enums/enums');
+const {QUANTITY, PRICE_DIFFERENCE} = require('./enums/enums');
 
 module.exports = async function(myPreviousOrder, currentQuote, previousQuote, EMA, executedPositions) {
 
@@ -10,12 +10,12 @@ module.exports = async function(myPreviousOrder, currentQuote, previousQuote, EM
   const didClose = shouldClose(currentQuote, executedPositions)
   if (Math.floor(currentQuote.lastPrice) >= Math.floor(EMA) +1  && previousQuote.lastPrice -1 <= EMA) {
     await operations.deleteAllOpen();
-    if( currentQty <=0){
+    if( currentQty <=0 ){
       const lp = livePosition(executedPositions);
       const quantity = (lp && lp.type === 'Sell')
         ? lp.currentQty * 2
         : QUANTITY;
-      myPreviousOrder = await operations.createOrder('XBTUSD', 'Buy', quantity, null, (currentQuote.lastPrice - 0.5), previousQuote.lastPrice);
+      myPreviousOrder = await operations.createOrder('XBTUSD', 'Buy', quantity, null, (currentQuote.lastPrice - PRICE_DIFFERENCE), previousQuote.lastPrice);
     }
   } else if (Math.ceil(currentQuote.lastPrice)  <= Math.ceil(EMA) -1 && previousQuote.lastPrice +1 >= EMA ) {
     await operations.deleteAllOpen();
@@ -24,7 +24,7 @@ module.exports = async function(myPreviousOrder, currentQuote, previousQuote, EM
       const quantity = (lp && lp.type === 'Buy')
         ? lp.currentQty * 2
         : QUANTITY;
-      myPreviousOrder = await operations.createOrder('XBTUSD', 'Sell', quantity, null, (currentQuote.lastPrice + 0.5), previousQuote.lastPrice);
+      myPreviousOrder = await operations.createOrder('XBTUSD', 'Sell', quantity, null, (currentQuote.lastPrice + PRICE_DIFFERENCE), previousQuote.lastPrice);
     }
   }
 }
